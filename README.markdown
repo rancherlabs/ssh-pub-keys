248069185d671ecd71e590d596285e3b2656fbf8
Nathan R Valentine <nrvale0@gmail.com>
2016-10-13 12:13:08 -0700

    initial

---
+++ README.markdown
@@ -0,0 +1,62 @@
+# SSH pub keys for Rancher Labs
+
+Text database of Rancher Labs public ssh keys.
+
+# Usage
+
+Use your favorite HTTP client to pull down a set of public SSH keys. The repo is organized
+such that you can simulate user groups via filesystem hierarchy.
+
+For instance, to download the pub key for user 'nrvale0' using [HTTPie](http://httpie.org):
+
+```ShellSession
+➜  ~ http https://raw.githubusercontent.com/rancherlabs/ssh-pub-keys/master/ssh-pub-keys/users/nrvale0
+HTTP/1.1 200 OK
+Accept-Ranges: bytes
+Access-Control-Allow-Origin: *
+Cache-Control: max-age=300
+Connection: keep-alive
+Content-Encoding: gzip
+Content-Length: 351
+Content-Security-Policy: default-src 'none'; style-src 'unsafe-inline'
+Content-Type: text/plain; charset=utf-8
+Date: Fri, 14 Oct 2016 21:10:51 GMT
+ETag: "c0b7df204c2da83a5c0154c6562593a1047566f4"
+Expires: Fri, 14 Oct 2016 21:15:51 GMT
+Source-Age: 56
+Strict-Transport-Security: max-age=31536000
+Vary: Authorization,Accept-Encoding
+Via: 1.1 varnish
+X-Cache: HIT
+X-Cache-Hits: 1
+X-Content-Type-Options: nosniff
+X-Fastly-Request-ID: 5390580b4919dbc5691a1ab7af6040bf8d1526fe
+X-Frame-Options: deny
+X-Geo-Block-List:
+X-GitHub-Request-Id: 689C5B1B:48FF:45E74F2:580149A3
+X-Served-By: cache-lax8635-LAX
+X-XSS-Protection: 1; mode=block
+
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDFGjXK1nKnGIIg7LERaDkBegCGFrTHgZ0d81f3kzifRINj2UjV9I5kWAMO9M3USfN0iwy/GFn95obmDj3M5gH+1/U8pP3UtP/J6XTrEcwiv63sSS+i1ib3cDeswoqTqDUbu89PncyfRCobkUHrXJWzY/5CEfXtDkBqCPMTQuV3sL+lueWHP1yV2HYSlpq/R+aS8y6DBx4lTRlkOODSRdmkWQg23HfpcWR3uzUeu5wcmVFEpjRbFwVwhLtARCYGVb5dPs65WbD+g6j47fpXjYcSTdSqa29QC5iQJDHOhcgnCVLlxybRLZA8nAtFgQyrFXRVYy2IDUStfo6KoreMkNT1 nrvale0
+```
+
+# Adding a pub key
+
+* Fork the repo.
+* Add your key to the appropriate files in the hierarchy
+* Optionally, do a local test run:
+
+```ShellSession
+➜ ~ dapper ./scripts/ci
+```
+Of course this implies the presence of [Dapper](https://github.com/rancher/dapper) - the Docker Build Wrapper.
+
+* If the test fails, fix your additions. ;)
+* Send in a Pull Request.
+
+# Contributors
+* Nathan Valentine - nathan@rancher.com | nrvale0@gmail.com | @nrvale0
+
+# Support / PRs / Issues
+
+[https://github.com/rancherlabs/ssh-pub-keys](https://github.com/rancherlabs/ssh-pub-keys)
---
+++ scripts/check_ssh_keys.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+import os, sys, re, colorama
+
+from colorama import Fore, Style, Back
+from invoke import run, Failure
+
+
+colorama.init()
+
+
+#
+def log_info(msg):
+    print(Back.BLACK + Fore.WHITE + msg + Fore.RESET)
+
+
+#
+def log_debug(msg):
+    print(Back.BLACK + Fore.BLUE + msg + Fore.RESET)
+
+
+#
+def log_error(msg):
+    sys.stderr.write(Back.BLACK + Style.BRIGHT + Fore.RED + msg + os.linesep + Fore.RESET + Style.NORMAL)
+
+
+#
+def log_success(msg):
+    print(Back.BLACK + Style.BRIGHT + Fore.GREEN + msg + Fore.RESET + Style.NORMAL)
+
+
+#
+def err_and_exit(msg):
+    log_error(msg)
+    sys.exit(-1)
+
+
+#
+def debug_mode():
+    if os.environ.get('DEBUG'):
+        return True
+    else:
+        return False
+
+
+#
+def validate_ssh_pub_key(line):
+    """
+    Validate the line passed as a valid pub key for user in authorized_keys* file.
+    This needs massive improvmement but is a 'good enough' start.
+    """
+
+    ssh_pub_key_re = '^(ssh\-dss|ssh\-rsa|ecdsa\-sha2\-nistp256|ecdsa\-sha2\-nistp384|ecdsa\-sha2\-nistp521).+$'
+    mo = re.match(ssh_pub_key_re, line)
+    if debug_mode():
+        log_debug("Checking \'{}\' against re \'{}\'.".format(line, ssh_pub_key_re))
+    if mo:
+        return True
+    else:
+        log_error("Failed to validate line \'{}\' against regex \'{}\'!".format(line, ssh_pub_key_re))
+        return False
+
+
+#
+def check_file(keysfile):
+    log_info("Checking \'{}\'...".format(keysfile))
+
+    result = True
+    lineno = 0
+    try:
+        with open(keysfile,'r') as f:
+            for line in f:
+                lineno += 1
+                line = line.rstrip()
+                if debug_mode():
+                    log_debug("Checking key at line {}...".format(lineno))
+                if False is validate_ssh_pub_key(line):
+                    log_error("Failed to validate line \'{}\' in file \'{}\' against regex \'{}\'!".format(line, keysfile))
+                    return False
+    except IOError as e:
+        log_error("Failed on open() for \'{}\' :: {} :: {}".format(
+            keysfile,
+            e.errno,
+            e.strerror))
+        return False
+
+    return True
+
+
+#
+def check_dir(keyspath):
+    result = True
+
+    for root, dirs, files in os.walk(keyspath, topdown=False):
+        if debug_mode():
+            log_debug("Checking: root: {}, dir: {}, files: {}...".format(root, dirs, files))
+            
+        for f in files:
+            if False is check_file("{}/{}".format(root, f)):
+                result = False
+            
+    return result
+
+
+#
+def main():
+    keydir = './ssh-pub-keys'
+
+    
+    log_info("Checking contents of \'{}\'...".format(keydir))
+    run("tree {}".format(keydir))
+
+    if False is check_dir(keydir):
+        err_and_exit('Failed while checking SSH keys!')
+    else:
+        log_success('Key files look good.')
+
+
+if '__main__' == __name__:
+    main()
